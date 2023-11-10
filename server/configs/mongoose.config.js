651fbcd6c4f19149b6d3bfd7983926d1fc0f924e
willfarmer35 <121136080+willfarmer35@users.noreply.github.com>
2023-11-09 16:39:29 -0800

    Add files via upload

--- server/configs/mongoose.config.js
+++ server/configs/mongoose.config.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose")
 
-const dbname = process.env.ATLAS_DATABASE
+
 const username = process.env.ATLAS_USERNAME
 const password = process.env.ATLAS_PASSWORD
 
-const uri = `mongodb+srv://willfarmer35:xeFKXFGocoezf0Es@cluster0.ukef31t.mongodb.net/userdb?retryWrites=true&w=majority`
+const uri = `mongodb+srv://${username}:${password}@cluster0.ukef31t.mongodb.net/userdb?retryWrites=true&w=majority`
 
 mongoose.connect(uri, {
     useNewUrlParser: true,
